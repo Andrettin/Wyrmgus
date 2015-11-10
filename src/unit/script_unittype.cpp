1d3f7a6911824358bb406abbd93ceb7ac6dea992
Andrettin <andre.ng@live.com>
2015-11-10 21:52:37 +0100

    Revert to issue caused by the previous commit making civilization association inherited from parent unit types

--- src/unit/script_unittype.cpp
+++ src/unit/script_unittype.cpp
@@ -605,7 +605,6 @@ static int CclDefineUnitType(lua_State *l)
 			if (!parent_type) {
 				LuaError(l, "Unit type %s not defined" _C_ type->Parent.c_str());
 			}
-			type->Civilization = parent_type->Civilization;
 			type->Class = parent_type->Class;
 			type->DrawLevel = parent_type->DrawLevel;
 			type->File = parent_type->File;
