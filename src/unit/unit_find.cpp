764b19d459dca54645a113303e0753afa1120d78
Andrettin <andre.ng@live.com>
2016-02-09 21:34:53 +0100

    Update from upstream

--- src/unit/unit_find.cpp
+++ src/unit/unit_find.cpp
@@ -1013,12 +1013,10 @@ public:
 				for (int xx = 0; xx < dtype.TileWidth; ++xx) {
 					int pos = (y + yy) * (size / 2) + (x + xx);
 					if (pos >= good->size()) {
-						DebugPrint(
-							"BUG: RangeTargetFinder::FillBadGood.Compute out of range. "\
-							"size: %d, pos: %d, "\
+						printf("BUG: RangeTargetFinder::FillBadGood.Compute out of range. "\
+							"size: %d, pos: %d, " \
 							"x: %d, xx: %d, y: %d, yy: %d",
-							size, pos, x, xx, y, yy
-						);
+							size, pos, x, xx, y, yy);
 						break;
 					}
 					if (cost < 0) {
@@ -1103,12 +1101,10 @@ private:
 				int pos = (y + yy) * (size / 2) + (x + xx);
 				int localFactor = (!xx && !yy) ? 1 : splashFactor;
 				if (pos >= good->size()) {
-					DebugPrint(
-						"BUG: RangeTargetFinder.Compute out of range. "\
-						"size: %d, pos: %d, "\
+					printf("BUG: RangeTargetFinder.Compute out of range. " \
+						"size: %d, pos: %d, "	\
 						"x: %d, xx: %d, y: %d, yy: %d",
-						size, pos, x, xx, y, yy
-					);
+						size, pos, x, xx, y, yy);
 					break;
 				}
 				sbad += bad->at(pos) / localFactor;
