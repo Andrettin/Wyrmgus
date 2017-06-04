d4312b68ca15f6e06dea8ae3e9ddaa24cb77d850
Andrettin <andre.ng@live.com>
2017-06-04 17:45:20 +0200

    Update to dehydration code

--- src/action/actions.cpp
+++ src/action/actions.cpp
@@ -502,8 +502,8 @@ static void HandleBuffsEachSecond(CUnit &unit)
 		
 		if (unit.Type->BoolFlag[ORGANIC_INDEX].value && Map.Info.IsPointOnMap(unit.tilePos.x, unit.tilePos.y, unit.MapLayer) && (Map.Field(unit.tilePos.x, unit.tilePos.y, unit.MapLayer)->Flags & MapFieldDesert) && Map.TimeOfDay[unit.MapLayer] >= DawnTimeOfDay && Map.TimeOfDay[unit.MapLayer] <= DuskTimeOfDay && unit.Variable[HYDRATING_INDEX].Value <= 0 && unit.Variable[DEHYDRATIONIMMUNITY_INDEX].Value <= 0) { //apply dehydration to an organic unit on a desert tile; only apply dehydration during day-time
 			unit.Variable[DEHYDRATION_INDEX].Enable = 1;
-			unit.Variable[DEHYDRATION_INDEX].Max = std::max(200, unit.Variable[DEHYDRATION_INDEX].Max);
-			unit.Variable[DEHYDRATION_INDEX].Value = std::max(200, unit.Variable[DEHYDRATION_INDEX].Value);
+			unit.Variable[DEHYDRATION_INDEX].Max = std::max(CYCLES_PER_SECOND + 1, unit.Variable[DEHYDRATION_INDEX].Max);
+			unit.Variable[DEHYDRATION_INDEX].Value = std::max(CYCLES_PER_SECOND + 1, unit.Variable[DEHYDRATION_INDEX].Value);
 		}
 	}
 	//Wyrmgus end
