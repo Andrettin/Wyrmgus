e550fbdb1686ce0a351858d5a5f8f9eb598236f3
Andrettin <andre.ng@live.com>
2017-08-28 14:26:57 +0200

    Added an error message if settlement sites can't be placed on their supposed location

--- src/map/map.cpp
+++ src/map/map.cpp
@@ -680,6 +680,9 @@ void CMapTemplate::ApplySettlements(Vec2i template_start_pos, Vec2i map_start_po
 
 		if (settlement_iterator->second->Major && SettlementSiteUnitType) { //add a settlement site for major settlements
 			Vec2i unit_offset((SettlementSiteUnitType->TileWidth - 1) / 2, (SettlementSiteUnitType->TileHeight - 1) / 2);
+			if (!UnitTypeCanBeAt(*SettlementSiteUnitType, settlement_pos - unit_offset, z)) {
+				fprintf(stderr, "The settlement site for \"%s\" should be placed on (%d, %d), but it cannot be there.\n", settlement_iterator->second->Ident.c_str(), settlement_raw_pos.x, settlement_raw_pos.y);
+			}
 			CUnit *unit = CreateUnit(settlement_pos - unit_offset, *SettlementSiteUnitType, &Players[PlayerNumNeutral], z);
 			unit->Settlement = settlement_iterator->second;
 			Map.SettlementUnits.push_back(unit);
@@ -806,6 +809,9 @@ void CMapTemplate::ApplySettlements(Vec2i template_start_pos, Vec2i map_start_po
 				if (first_building) {
 					if (!type->BoolFlag[TOWNHALL_INDEX].value && !unit->Unique && (!building_owner || building_owner == settlement_owner) && settlement_iterator->second->CulturalNames.find(settlement_owner->Civilization) != settlement_iterator->second->CulturalNames.end()) { //if one building is representing a minor settlement, make it have the settlement's name
 						unit->Name = settlement_iterator->second->CulturalNames.find(settlement_owner->Civilization)->second;
+					}
+					if (!OnTopDetails(*type, NULL) && !UnitTypeCanBeAt(*type, settlement_pos - unit_offset, z)) {
+						fprintf(stderr, "The \"%s\" representing the minor settlement of \"%s\" should be placed on (%d, %d), but it cannot be there.\n", type->Ident.c_str(), settlement_iterator->second->Ident.c_str(), settlement_raw_pos.x, settlement_raw_pos.y);
 					}
 					first_building = false;
 				}
