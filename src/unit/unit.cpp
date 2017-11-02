74fd3122a13873ae36749c3cc4ad89fb320344e4
Andrettin <andre.ng@live.com>
2017-11-02 16:12:13 +0100

    Further update to the learn ability code

--- src/unit/unit.cpp
+++ src/unit/unit.cpp
@@ -5969,7 +5969,7 @@ bool CUnit::CanLearnAbility(CUpgrade *ability, bool pre) const
 		if (!this->Character) {
 			return false;
 		}
-		if (!pre && !this->Character->Custom) {
+		if (!this->Character->Custom && this->GetIndividualUpgrade(ability) == 0) {
 			return false;
 		}
 		if (!pre && this->UpgradeRemovesExistingUpgrade(ability)) {
