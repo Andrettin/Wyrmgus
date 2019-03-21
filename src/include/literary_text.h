6b88613a430cfb74b9f7d30ccbf923f667fa74b8
Andrettin <andre.ng@live.com>
2019-03-21 07:36:14 +0100

    Made the literary text variables which are processing from config files into properties

--- src/include/literary_text.h
+++ src/include/literary_text.h
@@ -48,32 +48,37 @@ public:
 	
 	String GetName() const
 	{
-		return this->Name.c_str();
+		return this->Name;
+	}
+	
+	bool IsHidden() const
+	{
+		return this->Hidden;
 	}
 	
 	String GetAuthor() const
 	{
-		return this->Author.c_str();
+		return this->Author;
 	}
 	
 	String GetTranslator() const
 	{
-		return this->Translator.c_str();
+		return this->Translator;
 	}
 	
 	String GetPublisher() const
 	{
-		return this->Publisher.c_str();
+		return this->Publisher;
 	}
 	
 	String GetCopyrightNotice() const
 	{
-		return this->CopyrightNotice.c_str();
+		return this->CopyrightNotice;
 	}
 	
 	String GetNotes() const
 	{
-		return this->Notes.c_str();
+		return this->Notes;
 	}
 	
 	int GetYear() const
@@ -94,12 +99,58 @@ public:
 	CLiteraryTextChapter *GetChapter(const std::string &chapter_name) const;
 	
 private:
-	std::string Name;				/// name of the text
-	std::string Author;				/// author of the text
-	std::string Translator;			/// translator of the text
-	std::string Publisher;			/// publisher of the text
-	std::string CopyrightNotice;	/// copyright notice explaining that this text is in the public domain, or is licensed under an open-source license
-	std::string Notes;				/// notes to appear on the cover of the text
+	void SetName(const String name)
+	{
+		this->Name = name;
+	}
+
+	void SetHidden(const bool hidden)
+	{
+		this->Hidden = hidden;
+	}
+
+	void SetAuthor(const String author)
+	{
+		this->Author = author;
+	}
+
+	void SetTranslator(const String translator)
+	{
+		this->Translator = translator;
+	}
+
+	void SetPublisher(const String publisher)
+	{
+		this->Publisher = publisher;
+	}
+
+	void SetCopyrightNotice(const String copyright_notice)
+	{
+		this->CopyrightNotice = copyright_notice;
+	}
+
+	void SetNotes(const String notes)
+	{
+		this->Notes = notes;
+	}
+
+	void SetYear(const int year)
+	{
+		this->Year = year;
+	}
+
+	void SetInitialPage(const int initial_page)
+	{
+		this->InitialPage = initial_page;
+	}
+
+	String Name;					/// name of the text
+	bool Hidden = false;			/// whether the literary text is hidden
+	String Author;					/// author of the text
+	String Translator;				/// translator of the text
+	String Publisher;				/// publisher of the text
+	String CopyrightNotice;			/// copyright notice explaining that this text is in the public domain, or is licensed under an open-source license
+	String Notes;					/// notes to appear on the cover of the text
 	int Year = 0;					/// year of publication
 	int InitialPage = 1;			/// page in which the text begins
 	CIcon *Icon = nullptr;			/// the text's icon
--- src/stratagus/literary_text.cpp
+++ src/stratagus/literary_text.cpp
@@ -60,23 +60,7 @@ CLiteraryText::~CLiteraryText()
 */
 bool CLiteraryText::ProcessConfigDataProperty(const std::string &key, std::string value)
 {
-	if (key == "name") {
-		this->Name = value;
-	} else if (key == "author") {
-		this->Author = value;
-	} else if (key == "translator") {
-		this->Translator = value;
-	} else if (key == "publisher") {
-		this->Publisher = value;
-	} else if (key == "copyright_notice") {
-		this->CopyrightNotice = value;
-	} else if (key == "notes") {
-		this->Notes = value;
-	} else if (key == "year") {
-		this->Year = std::stoi(value);
-	} else if (key == "initial_page") {
-		this->InitialPage = std::stoi(value);
-	} else if (key == "icon") {
+	if (key == "icon") {
 		value = FindAndReplaceString(value, "_", "-");
 		this->Icon = CIcon::Get(value);
 	} else {
@@ -99,13 +83,33 @@ CLiteraryTextChapter *CLiteraryText::GetChapter(const std::string &chapter_name)
 
 void CLiteraryText::_bind_methods()
 {
+	ClassDB::bind_method(D_METHOD("_set_name", "name"), &CLiteraryText::SetName);
 	ClassDB::bind_method(D_METHOD("get_name"), &CLiteraryText::GetName);
+	ClassDB::bind_method(D_METHOD("_set_author", "author"), &CLiteraryText::SetAuthor);
 	ClassDB::bind_method(D_METHOD("get_author"), &CLiteraryText::GetAuthor);
+	ClassDB::bind_method(D_METHOD("_set_hidden", "hidden"), &CLiteraryText::SetHidden);
+	ClassDB::bind_method(D_METHOD("is_hidden"), &CLiteraryText::IsHidden);
+	ClassDB::bind_method(D_METHOD("_set_translator", "translator"), &CLiteraryText::SetTranslator);
 	ClassDB::bind_method(D_METHOD("get_translator"), &CLiteraryText::GetTranslator);
+	ClassDB::bind_method(D_METHOD("_set_publisher", "publisher"), &CLiteraryText::SetPublisher);
 	ClassDB::bind_method(D_METHOD("get_publisher"), &CLiteraryText::GetPublisher);
+	ClassDB::bind_method(D_METHOD("_set_copyright_notice", "copyright_notice"), &CLiteraryText::SetCopyrightNotice);
 	ClassDB::bind_method(D_METHOD("get_copyright_notice"), &CLiteraryText::GetCopyrightNotice);
+	ClassDB::bind_method(D_METHOD("_set_notes", "notes"), &CLiteraryText::SetNotes);
 	ClassDB::bind_method(D_METHOD("get_notes"), &CLiteraryText::GetNotes);
+	ClassDB::bind_method(D_METHOD("_set_year", "year"), &CLiteraryText::SetYear);
 	ClassDB::bind_method(D_METHOD("get_year"), &CLiteraryText::GetYear);
+	ClassDB::bind_method(D_METHOD("_set_initial_page", "initial_page"), &CLiteraryText::SetInitialPage);
 	ClassDB::bind_method(D_METHOD("get_initial_page"), &CLiteraryText::GetInitialPage);
 	ClassDB::bind_method(D_METHOD("get_icon"), &CLiteraryText::GetIcon);
+	
+	ADD_PROPERTY(PropertyInfo(Variant::STRING, "name"), "_set_name", "get_name");
+	ADD_PROPERTY(PropertyInfo(Variant::STRING, "author"), "_set_author", "get_author");
+	ADD_PROPERTY(PropertyInfo(Variant::STRING, "hidden"), "_set_hidden", "is_hidden");
+	ADD_PROPERTY(PropertyInfo(Variant::STRING, "translator"), "_set_translator", "get_translator");
+	ADD_PROPERTY(PropertyInfo(Variant::STRING, "publisher"), "_set_publisher", "get_publisher");
+	ADD_PROPERTY(PropertyInfo(Variant::STRING, "copyright_notice"), "_set_copyright_notice", "get_copyright_notice");
+	ADD_PROPERTY(PropertyInfo(Variant::STRING, "notes"), "_set_notes", "get_notes");
+	ADD_PROPERTY(PropertyInfo(Variant::INT, "year"), "_set_year", "get_year");
+	ADD_PROPERTY(PropertyInfo(Variant::INT, "initial_page"), "_set_initial_page", "get_initial_page");
 }
