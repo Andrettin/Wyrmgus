4064ad7877adf8fa6e209b6d6c7d1545b9f74323
Andrettin <andre.ng@live.com>
2019-04-27 21:05:30 +0200

    Added signal for when Wyrmgus has finished its initialization

--- wyrmgus.cpp
+++ wyrmgus.cpp
@@ -117,6 +117,7 @@ void Wyrmgus::_bind_methods()
 
 	ClassDB::bind_method(D_METHOD("number_to_roman_numeral", "number"), &Wyrmgus::NumberToRomanNumeral);
 	
+	ADD_SIGNAL(MethodInfo("initialized"));
 	ADD_SIGNAL(MethodInfo("this_player_changed", PropertyInfo(Variant::OBJECT, "old_player"), PropertyInfo(Variant::OBJECT, "new_player")));
 	ADD_SIGNAL(MethodInfo("interface_changed", PropertyInfo(Variant::STRING, "old_interface"), PropertyInfo(Variant::STRING, "new_interface")));
 	
