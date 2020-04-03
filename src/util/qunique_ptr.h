755304b5c6ce6ae394c46041a75f43e58bf7230b
Andrettin <6322423+Andrettin@users.noreply.github.com>
2020-04-03 23:45:25 +0200

    Added qunique_ptr type alias

---
+++ src/util/qunique_ptr.h
@@ -0,0 +1,58 @@
+//       _________ __                 __
+//      /   _____//  |_____________ _/  |______     ____  __ __  ______
+//      \_____  \\   __\_  __ \__  \\   __\__  \   / ___\|  |  \/  ___/
+//      /        \|  |  |  | \// __ \|  |  / __ \_/ /_/  >  |  /\___ |
+//     /_______  /|__|  |__|  (____  /__| (____  /\___  /|____//____  >
+//             \/                  \/          \//_____/            \/
+//  ______________________                           ______________________
+//                        T H E   W A R   B E G I N S
+//         Stratagus - A free fantasy real time strategy game engine
+//
+//      (c) Copyright 2020 by Andrettin
+//
+//      Permission is hereby granted, free of charge, to any person obtaining a
+//      copy of this software and associated documentation files (the
+//      "Software"), to deal in the Software without restriction, including
+//      without limitation the rights to use, copy, modify, merge, publish,
+//      distribute, sublicense, and/or sell copies of the Software, and to
+//      permit persons to whom the Software is furnished to do so, subject to
+//      the following conditions:
+//
+//      The above copyright notice and this permission notice shall be included
+//      in all copies or substantial portions of the Software.
+//
+//      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+//      OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//      MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+//      IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+//      CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+//      TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+//      SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#pragma once
+
+#include <QObject>
+
+#include <memory>
+
+namespace stratagus {
+
+struct qobject_deleter
+{
+	void operator()(QObject *object)
+	{
+		object->deleteLater();
+	}
+};
+
+//unique pointer that calls QObject::deleteLater
+template <typename T>
+using qunique_ptr = std::unique_ptr<T, qobject_deleter>;
+
+template <typename T, class... ARGS>
+inline qunique_ptr<T> make_qunique(ARGS&&... args)
+{
+	return qunique_ptr<T>(new T(std::forward<ARGS>(args)...));
+}
+
+}
