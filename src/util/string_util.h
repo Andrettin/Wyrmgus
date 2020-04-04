6ca4584512670704641781ac932bd07c92fb2b39
Andrettin <6322423+Andrettin@users.noreply.github.com>
2020-04-04 19:27:30 +0200

    Added string utility function to split the string by a delimiter into a string list

--- src/util/string_util.h
+++ src/util/string_util.h
@@ -38,6 +38,24 @@ namespace string {
 
 static inline const std::string empty_str;
 
+inline std::vector<std::string> split(const std::string &str, const char delimiter)
+{
+	std::vector<std::string> string_list;
+
+	size_t start_pos = 0;
+	size_t find_pos = 0;
+	while ((find_pos = str.find(delimiter, start_pos)) != std::string::npos) {
+		std::string string_element = str.substr(start_pos, find_pos - start_pos);
+		string_list.push_back(string_element);
+		start_pos = find_pos + 1;
+	}
+
+	std::string string_element = str.substr(start_pos, str.length() - start_pos);
+	string_list.push_back(string_element);
+
+	return string_list;
+}
+
 inline bool to_bool(const std::string &str)
 {
 	if (str == "true" || str == "yes" || str == "1") {
