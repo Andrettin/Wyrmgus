4fbae39034194a0a52c621ea8f67b7518918122a
Andrettin <6322423+Andrettin@users.noreply.github.com>
2020-05-16 22:48:51 +0200

    Version update

--- src/include/version.h
+++ src/include/version.h
@@ -4,9 +4,9 @@
 /// Description
 #define DESCRIPTION NAME " - Strategy Gaming Engine"
 
-#define StratagusMajorVersion 3
-#define StratagusMinorVersion 5
-#define StratagusPatchLevel 5
+#define StratagusMajorVersion 4
+#define StratagusMinorVersion 0
+#define StratagusPatchLevel 0
 #define StratagusPatchLevel2 0
 
 #define _version_stringify_(s) #s
@@ -33,4 +33,4 @@
 #define LICENSE "GPL 2.0"
 
 /// Copyright
-#define COPYRIGHT "Copyright (c) 1998-2015 by The Stratagus Project"
+#define COPYRIGHT "Copyright (c) 1998-2020 by The Stratagus Project"
