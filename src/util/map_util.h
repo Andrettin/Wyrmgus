907852299e748c0a3713cc2bab4f7bd4f791f602
Andrettin <6322423+Andrettin@users.noreply.github.com>
2020-06-12 18:44:47 +0200

    Added utility functions to remove values from maps

---
+++ src/util/map_util.h
@@ -0,0 +1,60 @@
+//       _________ __                 __
+//      /   _____//  |_____________ _/  |______     ____  __ __  ______
+//      \_____  \\   __\_  __ \__  \\   __\__  \   / ___\|  |  \/  ___/
+//      /        \|  |  |  | \// __ \|  |  / __ \_/ /_/  >  |  /\___ |
+//     /_______  /|__|  |__|  (____  /__| (____  /\___  /|____//____  >
+//             \/                  \/          \//_____/            \/
+//  ______________________                           ______________________
+//                        T H E   W A R   B E G I N S
+//         Stratagus - A free fantasy real time strategy game engine
+//
+//      (c) Copyright 2020 by Andrettin
+//
+//      Permission is hereby granted, free of charge, to any person obtaining a
+//      copy of this software and associated documentation files (the
+//      "Software"), to deal in the Software without restriction, including
+//      without limitation the rights to use, copy, modify, merge, publish,
+//      distribute, sublicense, and/or sell copies of the Software, and to
+//      permit persons to whom the Software is furnished to do so, subject to
+//      the following conditions:
+//
+//      The above copyright notice and this permission notice shall be included
+//      in all copies or substantial portions of the Software.
+//
+//      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+//      OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//      MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+//      IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+//      CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+//      TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+//      SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#pragma once
+
+namespace stratagus::map {
+
+template <typename T>
+inline void remove_value(T &map, const typename T::value_type &value)
+{
+	for (auto it = map.begin(); it != map.end();) {
+		if ((*it).second == value) {
+			it = map.erase(it);
+		} else {
+			it++;
+		}
+	}
+}
+
+template <typename T, typename function_type>
+inline void remove_value_if(T &map, const function_type &function)
+{
+	for (auto it = map.begin(); it != map.end();) {
+		if (function((*it).second)) {
+			it = map.erase(it);
+		} else {
+			it++;
+		}
+	}
+}
+
+}
