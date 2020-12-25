07623ff23c0df847749223be2bcd5d72423fb8a3
Andrettin <6322423+Andrettin@users.noreply.github.com>
2020-12-25 22:35:21 +0100

    Version update

--- src/include/version.h
+++ src/include/version.h
@@ -7,7 +7,7 @@
 #define StratagusMajorVersion 4
 #define StratagusMinorVersion 0
 #define StratagusPatchLevel 0
-#define StratagusPatchLevel2 2
+#define StratagusPatchLevel2 3
 
 #define _version_stringify_(s) #s
 #define _version_stringify(s) _version_stringify_(s)
