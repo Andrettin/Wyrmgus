5b7b14f5e7bde423bb9042e23a44efe90a41e13e
Andrettin <6322423+Andrettin@users.noreply.github.com>
2021-01-25 13:24:10 +0100

    Added unit tests for the AU/GM and LY/PC conversions

---
+++ test/util/astronomy_test.cpp
@@ -0,0 +1,66 @@
+//       _________ __                 __
+//      /   _____//  |_____________ _/  |______     ____  __ __  ______
+//      \_____  \\   __\_  __ \__  \\   __\__  \   / ___\|  |  \/  ___/
+//      /        \|  |  |  | \// __ \|  |  / __ \_/ /_/  >  |  /\___ |
+//     /_______  /|__|  |__|  (____  /__| (____  /\___  /|____//____  >
+//             \/                  \/          \//_____/            \/
+//  ______________________                           ______________________
+//                        T H E   W A R   B E G I N S
+//         Stratagus - A free fantasy real time strategy game engine
+//
+//      (c) Copyright 2021 by Andrettin
+//
+//      This program is free software; you can redistribute it and/or modify
+//      it under the terms of the GNU General Public License as published by
+//      the Free Software Foundation; only version 2 of the License.
+//
+//      This program is distributed in the hope that it will be useful,
+//      but WITHOUT ANY WARRANTY; without even the implied warranty of
+//      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//      GNU General Public License for more details.
+//
+//      You should have received a copy of the GNU General Public License
+//      along with this program; if not, write to the Free Software
+//      Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+//      02111-1307, USA.
+//
+
+#include "util/astronomy_util.h"
+
+#include "util/fractional_int.h"
+
+#include <boost/test/unit_test.hpp>
+
+using namespace wyrmgus;
+
+BOOST_AUTO_TEST_CASE(pc_to_ly_test)
+{
+    const centesimal_int pc("76.28");
+    const centesimal_int ly = astronomy::pc_to_ly(pc);
+
+    BOOST_CHECK(ly == centesimal_int("248.67"));
+}
+
+BOOST_AUTO_TEST_CASE(ly_to_pc_test)
+{
+    const centesimal_int ly(382);
+    const centesimal_int pc = astronomy::ly_to_pc(ly);
+
+    BOOST_CHECK(pc == centesimal_int("117.17"));
+}
+
+BOOST_AUTO_TEST_CASE(au_to_gm_test)
+{
+    const int au = 23;
+    const int gm = astronomy::au_to_gm(au);
+
+    BOOST_CHECK(gm == 3450);
+}
+
+BOOST_AUTO_TEST_CASE(gm_to_au_test)
+{
+    const int gm = 778;
+    const int au = astronomy::gm_to_au(gm);
+
+    BOOST_CHECK(au == 5);
+}
