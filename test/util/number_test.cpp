b1cf51d2c262bbe0e80388c469f0e2570cc30712
Andrettin <6322423+Andrettin@users.noreply.github.com>
2021-02-20 10:46:51 +0100

    Enhanced the icbrt test, and added an isqrt test

--- test/util/number_test.cpp
+++ test/util/number_test.cpp
@@ -27,15 +27,44 @@
 #include "stratagus.h"
 
 #include "util/number_util.h"
+#include "util/util.h"
 
 #include <boost/test/unit_test.hpp>
 
+BOOST_AUTO_TEST_CASE(isqrt_test)
+{
+    BOOST_CHECK(isqrt(1) == 1);
+    BOOST_CHECK(isqrt(2) == 1);
+    BOOST_CHECK(isqrt(3) == 1);
+    BOOST_CHECK(isqrt(4) == 2);
+    BOOST_CHECK(isqrt(5) == 2);
+    BOOST_CHECK(isqrt(6) == 2);
+    BOOST_CHECK(isqrt(7) == 2);
+    BOOST_CHECK(isqrt(8) == 2);
+    BOOST_CHECK(isqrt(9) == 3);
+    BOOST_CHECK(isqrt(10) == 3);
+    BOOST_CHECK(isqrt(27) == 5);
+    BOOST_CHECK(isqrt(32) == 5);
+    BOOST_CHECK(isqrt(64) == 8);
+    BOOST_CHECK(isqrt(778) == 27);
+    BOOST_CHECK(isqrt(75900) == 275);
+}
+
 BOOST_AUTO_TEST_CASE(icbrt_test)
 {
+    BOOST_CHECK(number::cbrt(1) == 1);
+    BOOST_CHECK(number::cbrt(2) == 1);
+    BOOST_CHECK(number::cbrt(3) == 1);
+    BOOST_CHECK(number::cbrt(4) == 1);
+    BOOST_CHECK(number::cbrt(5) == 1);
+    BOOST_CHECK(number::cbrt(6) == 1);
+    BOOST_CHECK(number::cbrt(7) == 1);
     BOOST_CHECK(number::cbrt(8) == 2);
     BOOST_CHECK(number::cbrt(9) == 2);
     BOOST_CHECK(number::cbrt(10) == 2);
     BOOST_CHECK(number::cbrt(27) == 3);
     BOOST_CHECK(number::cbrt(32) == 3);
     BOOST_CHECK(number::cbrt(64) == 4);
+    BOOST_CHECK(number::cbrt(778) == 9);
+    BOOST_CHECK(number::cbrt(75900) == 42);
 }
