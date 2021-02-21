82a5324b2b72c666613f66d97dd26b047bece174
Andrettin <6322423+Andrettin@users.noreply.github.com>
2021-02-21 18:46:41 +0100

    Updated the isqrt and icbrt tests

--- test/util/number_test.cpp
+++ test/util/number_test.cpp
@@ -44,14 +44,21 @@ static void check_sqrt()
     BOOST_CHECK(isqrt<number_type>(8) == 2);
     BOOST_CHECK(isqrt<number_type>(9) == 3);
     BOOST_CHECK(isqrt<number_type>(10) == 3);
+    BOOST_CHECK(isqrt<number_type>(18) == 4);
     BOOST_CHECK(isqrt<number_type>(27) == 5);
     BOOST_CHECK(isqrt<number_type>(32) == 5);
     BOOST_CHECK(isqrt<number_type>(64) == 8);
+    BOOST_CHECK(isqrt<number_type>(100) == 10);
+    BOOST_CHECK(isqrt<number_type>(101) == 10);
 
     if constexpr (static_cast<uint64_t>(std::numeric_limits<number_type>::max()) >= static_cast<uint64_t>(std::numeric_limits<int16_t>::max())) {
         BOOST_CHECK(isqrt<number_type>(317) == 17);
+        BOOST_CHECK(isqrt<number_type>(700) == 26);
         BOOST_CHECK(isqrt<number_type>(778) == 27);
+        BOOST_CHECK(isqrt<number_type>(1000) == 31);
+        BOOST_CHECK(isqrt<number_type>(1024) == 32);
         BOOST_CHECK(isqrt<number_type>(1030) == 32);
+        BOOST_CHECK(isqrt<number_type>(2900) == 53);
     }
 
     if constexpr (static_cast<uint64_t>(std::numeric_limits<number_type>::max()) >= static_cast<uint64_t>(std::numeric_limits<int32_t>::max())) {
@@ -116,14 +123,21 @@ static void check_cbrt()
     BOOST_CHECK(number::cbrt<number_type>(8) == 2);
     BOOST_CHECK(number::cbrt<number_type>(9) == 2);
     BOOST_CHECK(number::cbrt<number_type>(10) == 2);
+    BOOST_CHECK(number::cbrt<number_type>(18) == 2);
     BOOST_CHECK(number::cbrt<number_type>(27) == 3);
     BOOST_CHECK(number::cbrt<number_type>(32) == 3);
     BOOST_CHECK(number::cbrt<number_type>(64) == 4);
+    BOOST_CHECK(number::cbrt<number_type>(100) == 4);
+    BOOST_CHECK(number::cbrt<number_type>(101) == 4);
 
     if constexpr (static_cast<uint64_t>(std::numeric_limits<number_type>::max()) >= static_cast<uint64_t>(std::numeric_limits<int16_t>::max())) {
         BOOST_CHECK(number::cbrt<number_type>(317) == 6);
+        BOOST_CHECK(number::cbrt<number_type>(700) == 8);
         BOOST_CHECK(number::cbrt<number_type>(778) == 9);
+        BOOST_CHECK(number::cbrt<number_type>(1000) == 10);
+        BOOST_CHECK(number::cbrt<number_type>(1024) == 10);
         BOOST_CHECK(number::cbrt<number_type>(1030) == 10);
+        BOOST_CHECK(number::cbrt<number_type>(2900) == 14);
     }
 
     if constexpr (static_cast<uint64_t>(std::numeric_limits<number_type>::max()) >= static_cast<uint64_t>(std::numeric_limits<int32_t>::max())) {
