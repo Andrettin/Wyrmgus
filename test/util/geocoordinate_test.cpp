4f771b0a02c1bdb931f55fc4fe327647cde0711f
Andrettin <6322423+Andrettin@users.noreply.github.com>
2021-02-27 08:08:17 +0100

    Added test for converting points to geocoordinates

--- test/util/geocoordinate_test.cpp
+++ test/util/geocoordinate_test.cpp
@@ -28,6 +28,8 @@
 
 #include "util/geocoordinate.h"
 
+#include "map/equirectangular_map_projection.h"
+#include "util/georectangle_util.h"
 #include "util/point_util.h"
 
 #include <boost/test/unit_test.hpp>
@@ -48,4 +50,25 @@ BOOST_AUTO_TEST_CASE(geocoordinate_to_circle_edge_point_test_2)
 
     const QPoint expected_result(-7071, -7071);
     BOOST_TEST((direction_pos == expected_result), "direction_pos " + point::to_string(direction_pos) + " does not equal " + point::to_string(expected_result));
+}
+
+BOOST_AUTO_TEST_CASE(point_to_geocoordinate_test)
+{
+    const map_projection *map_projection = equirectangular_map_projection::get();
+    const QSize size(384, 192);
+
+    const int min_longitude = -11;
+    const int max_longitude = 34;
+    const int min_latitude = 45;
+    const int max_latitude = 60;
+    const QRect georectangle(QPoint(min_longitude, min_latitude), QPoint(max_longitude, max_latitude));
+    const QRect unsigned_georectangle = georectangle::to_unsigned_georectangle(georectangle);
+
+    geocoordinate geocoordinate = map_projection->point_to_geocoordinate(QPoint(0, 0), size, unsigned_georectangle);
+    BOOST_CHECK(geocoordinate.get_longitude() == min_longitude);
+    BOOST_CHECK(geocoordinate.get_latitude() == max_latitude);
+
+    geocoordinate = map_projection->point_to_geocoordinate(QPoint(383, 191), size, unsigned_georectangle);
+    BOOST_CHECK(geocoordinate.get_longitude().to_int() == (max_longitude - 1));
+    BOOST_CHECK(geocoordinate.get_latitude().to_int() == min_latitude);
 }
