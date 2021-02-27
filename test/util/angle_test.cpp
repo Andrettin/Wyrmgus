13af8f3e10dc05cf2d201cc2230418329c01736a
Andrettin <6322423+Andrettin@users.noreply.github.com>
2021-02-27 09:55:11 +0100

    Added utility functions to convert between radians and degrees, along with tests for them

---
+++ test/util/angle_test.cpp
@@ -0,0 +1,45 @@
+//       _________ __                 __
+//      /   _____//  |_____________ _/  |______     ____  __ __  ______
+//      \_____  \\   __\_  __ \__  \\   __\__  \   / ___\|  |  \/  ___/
+//      /        \|  |  |  | \// __ \|  |  / __ \_/ /_/  >  |  /\___ |
+//     /_______  /|__|  |__|  (____  /__| (____  /\___  /|____//____  >
+//             \/                  \/          \//_____/            \/
+//  ______________________                           ______________________
+//                        T H E   W A R   B E G I N S
+//         Stratagus - A free fantasy real time strategy game engine
+//
+//      (c) Copyright 2021 by Andrettin
+//
+//      This program is free software; you can redistribute it and/or modify
+//      it under the terms of the GNU General Public License as published by
+//      the Free Software Foundation; only version 2 of the License.
+//
+//      This program is distributed in the hope that it will be useful,
+//      but WITHOUT ANY WARRANTY; without even the implied warranty of
+//      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//      GNU General Public License for more details.
+//
+//      You should have received a copy of the GNU General Public License
+//      along with this program; if not, write to the Free Software
+//      Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+//      02111-1307, USA.
+
+#include "stratagus.h"
+
+#include "util/angle_util.h"
+
+#include "util/fractional_int.h"
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(degrees_to_radians_test)
+{
+    BOOST_CHECK(angle::degrees_to_radians(angle::number_type(180)) == angle::pi);
+    BOOST_CHECK(angle::degrees_to_radians(angle::number_type(90)) == angle::number_type("1.5708"));
+}
+
+BOOST_AUTO_TEST_CASE(radians_to_degrees_test)
+{
+    BOOST_CHECK(angle::radians_to_degrees(angle::pi) == 180);
+    BOOST_CHECK(angle::radians_to_degrees(angle::number_type("1.5708")) == 90);
+}
