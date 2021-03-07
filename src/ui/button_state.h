4d4eb8f785634bb0e56421c05d6bf99e6ddb955d
Andrettin <6322423+Andrettin@users.noreply.github.com>
2021-03-07 09:00:56 +0100

    Added button state enum

---
+++ src/ui/button_state.h
@@ -0,0 +1,50 @@
+//       _________ __                 __
+//      /   _____//  |_____________ _/  |______     ____  __ __  ______
+//      \_____  \\   __\_  __ \__  \\   __\__  \   / ___\|  |  \/  ___/
+//      /        \|  |  |  | \// __ \|  |  / __ \_/ /_/  >  |  /\___ |
+//     /_______  /|__|  |__|  (____  /__| (____  /\___  /|____//____  >
+//             \/                  \/          \//_____/            \/
+//  ______________________                           ______________________
+//                        T H E   W A R   B E G I N S
+//         Stratagus - A free fantasy real time strategy game engine
+//
+//      (c) Copyright 2021 by Andrettin
+//
+//      This program is free software; you can redistribute it and/or modify
+//      it under the terms of the GNU General Public License as published by
+//      the Free Software Foundation; only version 2 of the License.
+//
+//      This program is distributed in the hope that it will be useful,
+//      but WITHOUT ANY WARRANTY; without even the implied warranty of
+//      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//      GNU General Public License for more details.
+//
+//      You should have received a copy of the GNU General Public License
+//      along with this program; if not, write to the Free Software
+//      Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+//      02111-1307, USA.
+
+#pragma once
+
+namespace wyrmgus {
+
+enum class button_state {
+	normal,
+	pressed,
+	grayed
+};
+
+inline button_state string_to_button_state(const std::string &str)
+{
+	if (str == "normal") {
+		return button_state::normal;
+	} else if (str == "pressed") {
+		return button_state::pressed;
+	} else if (str == "grayed") {
+		return button_state::grayed;
+	}
+
+	throw std::runtime_error("Invalid button state: \"" + str + "\".");
+}
+
+}
