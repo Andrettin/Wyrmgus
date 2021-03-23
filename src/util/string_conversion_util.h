d92fd4dca8f7d8619a6d693ebf3efdd1be74a085
Andrettin <6322423+Andrettin@users.noreply.github.com>
2021-03-23 18:37:57 +0100

    Added utility function to convert a boolean value to a string

--- src/util/string_conversion_util.h
+++ src/util/string_conversion_util.h
@@ -39,6 +39,15 @@ inline bool to_bool(const std::string &str)
 	throw std::runtime_error("Invalid string used for conversion to boolean: \"" + str + "\".");
 }
 
+inline std::string from_bool(const bool value)
+{
+	if (value) {
+		return "true";
+	} else {
+		return "false";
+	}
+}
+
 inline char to_character(const std::string &str)
 {
 	if (str.size() != 1) {
