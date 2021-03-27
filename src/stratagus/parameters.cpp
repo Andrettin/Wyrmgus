87612f06dc5255f6e218822d7ed6873288c28c91
Anton Karmanov <a.karmanov@inventati.org>
2021-03-28 01:02:30 +0300

    Add long args variants

--- src/stratagus/parameters.cpp
+++ src/stratagus/parameters.cpp
@@ -43,40 +43,43 @@ void parameters::process()
 {
 	QCommandLineParser cmd_parser;
 
-	// TODO Long version
 	QList<QCommandLineOption> options {
-		{ "e", "Start editor instead of game." },
-		{ "D", "Video mode depth (pixel per point).", "depth" },
-		{ "c", "Configuration start file (default is 'stratagus.lua').", "config file" },
-		{ "E", "Editor configuration start file (default is 'editor.lua').", "editor config file" },
-		{ "d", "Specify a custom data path.", "data path" },
-		{ "t", "Check startup and exit (data files must respect this flag)." },
-		{ "F", "Full screen video mode." },
-		{ "G", "Game options passed to game scripts", "game options." },
-		{ "I", "Network address to use", "address." },
-		{ "l", "Disable command log." },
-		{ "N", "Name of the player.", "name" },
+		{ { "e", "editor" }, "Start editor instead of game." },
+		{ { "D", "video-depth" }, "Video mode depth (pixel per point).", "depth" },
+		{ { "c", "config" }, "Configuration start file (default is 'stratagus.lua').", "config file" },
+		{
+			{ "E", "editor-config" },
+			"Editor configuration start file (default is 'editor.lua').",
+			"editor config file"
+		},
+		{ { "d", "data-path" }, "Specify a custom data path.", "data path" },
+		{ { "t", "test-run" }, "Check startup and exit (data files must respect this flag)." },
+		{ { "F", "full-screen" }, "Full screen video mode." },
+		{ { "G", "game-options" }, "Game options passed to game scripts", "game options" },
+		{ { "I", "ip-address" }, "Network address to use", "address" },
+		{ { "l", "no-command-log" }, "Disable command log." },
+		{ { "N", "player-name" }, "Name of the player.", "name" },
 #if defined(USE_OPENGL) || defined(USE_GLES)
-		{ "o", "Do not use OpenGL or OpenGL ES 1.1." },
-		{ "O", "Use OpenGL or OpenGL ES 1.1." },
+		{ { "o", "no-opengl" }, "Do not use OpenGL or OpenGL ES 1.1." },
+		{ { "O", "opengl" }, "Use OpenGL or OpenGL ES 1.1." },
 #endif
-		{ "p", "Enables debug messages printing in console." },
-		{ "P", "Network port to use.", "port" },
-		{ "s", "Number of frames for the AI to sleep before it starts.", "frames" },
-		{ "S", "Sync speed (100 = 30 frames/s).", "speed" },
-		{ "u", "Path where wyrmgus saves preferences, log and savegame", "path" },
-		{ "m", "Video mode resolution in format <xres>x<yres>.", "mode" },
-		{ "W", "Windowed video mode." },
+		{ { "p", "print-debug" }, "Enables debug messages printing in console." },
+		{ { "P", "port" }, "Network port to use.", "port" },
+		{ { "s", "sleep" }, "Number of frames for the AI to sleep before it starts.", "frames" },
+		{ { "S", "sync-rate" }, "Sync speed (100 = 30 frames/s).", "speed" },
+		{ { "u", "user-path" }, "Path where wyrmgus saves preferences, log and savegame", "path" },
+		{ { "m", "video-mode" }, "Video mode resolution in format <xres>x<yres>.", "mode" },
+		{ { "W", "windowed" }, "Windowed video mode." },
 #if defined(USE_OPENGL) || defined(USE_GLES)
 		{
-			"x",
+			{ "x", "scaling-mode" },
 			"Controls fullscreen scaling if your graphics card supports shaders. "
 				"Pass 1 for nearest-neigubour, 2 for EPX/AdvMame, 3 for HQx, 4 for SAL, 5 for SuperEagle. "
 				"You can also use Ctrl+Alt+/ to cycle between these scaling algorithms at runtime. "
 				"Pass -1 to force old-school nearest neighbour scaling without shaders.",
 			"idx"
 		},
-		{ "Z", "Use OpenGL to scale the screen to the viewport (retro-style). Implies -O." },
+		{ { "Z", "retro-scale" }, "Use OpenGL to scale the screen to the viewport (retro-style). Implies -O." },
 #endif
 	};
 	cmd_parser.addOptions(options);
@@ -98,7 +101,6 @@ void parameters::process()
 		this->test_run = true;
 	}
 
-	// TODO
 	if (cmd_parser.isSet("D")) {
 		Video.Depth = cmd_parser.value("D").toInt();
 	}
