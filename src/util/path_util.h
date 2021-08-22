a79fceb7b3719ea1b4549e935db90f507777739e
Andrettin <6322423+Andrettin@users.noreply.github.com>
2021-08-22 16:55:34 +0200

    Added the path::to_string and path::to_qstring utility functions, to make it so paths are converted to UTF-8 strings

--- src/util/path_util.h
+++ src/util/path_util.h
@@ -26,4 +26,20 @@
 
 #pragma once
 
+namespace wyrmgus::path {
+
+inline std::string to_string(const std::filesystem::path &path)
+{
+	//convert a path to a UTF-8 encoded string
+	const std::u8string u8str = path.u8string();
+	return std::string(u8str.begin(), u8str.end());
+}
+
+inline QString to_qstring(const std::filesystem::path &path)
+{
+	return QString::fromStdString(path::to_string(path));
+}
+
+}
+
 Q_DECLARE_METATYPE(std::filesystem::path)
