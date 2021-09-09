5ee4c0a97203351ed55c5fa5ed1fb0347c7ec371
Andrettin <6322423+Andrettin@users.noreply.github.com>
2021-09-09 19:24:20 +0200

    Added support for getting whether a player is an ally, is an enemy or has shared with vision another player via the GetPlayerData() Lua function

--- src/player/script_player.cpp
+++ src/player/script_player.cpp
@@ -2453,6 +2453,21 @@ static int CclGetPlayerData(lua_State *l)
 		}
 		return 1;
 	//Wyrmgus start
+	} else if (!strcmp(data, "IsAllied")) {
+		LuaCheckArgs(l, 3);
+		int second_player = LuaToNumber(l, 3);
+		lua_pushboolean(l, p->IsAllied(second_player));
+		return 1;
+	} else if (!strcmp(data, "IsEnemy")) {
+		LuaCheckArgs(l, 3);
+		int second_player = LuaToNumber(l, 3);
+		lua_pushboolean(l, p->IsEnemy(second_player));
+		return 1;
+	} else if (!strcmp(data, "HasSharedVisionWith")) {
+		LuaCheckArgs(l, 3);
+		int second_player = LuaToNumber(l, 3);
+		lua_pushboolean(l, p->has_shared_vision_with(second_player));
+		return 1;
 	} else if (!strcmp(data, "HasContactWith")) {
 		LuaCheckArgs(l, 3);
 		int second_player = LuaToNumber(l, 3);
