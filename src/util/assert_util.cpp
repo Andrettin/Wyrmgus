5672b0e235f5d141edc67b7d53e3e2252b8da040
Andrettin <6322423+Andrettin@users.noreply.github.com>
2021-10-11 18:32:27 +0200

    Added assert_log function

--- src/util/assert_util.cpp
+++ src/util/assert_util.cpp
@@ -24,49 +24,25 @@
 //      Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
 //      02111-1307, USA.
 
-#pragma once
+#include "stratagus.h"
+
+#include "util/assert_util.h"
+
+#include "util/log_util.h"
 
 namespace wyrmgus {
 
-inline std::string get_source_relative_filepath_string(const std::string &filepath)
-{
-	size_t pos = filepath.find("src");
-	if (pos != std::string::npos) {
-		pos += 4;
-		return filepath.substr(pos, filepath.size() - pos);
-	}
-
-	return filepath;
-}
-
 #ifdef __cpp_lib_source_location
-inline std::string get_assert_message(const std::source_location &location)
-{
-	return "Assert failed at " + get_source_relative_filepath_string(location.file_name()) + ": " + std::to_string(location.line()) + ", " + location.function_name() + ".";
-}
-#endif
-
-#ifdef __cpp_lib_source_location
-inline void assert_throw(const bool check, const std::source_location &location = std::source_location::current())
+void assert_log(const bool check, const std::source_location &location)
 #else
-inline void assert_throw(const bool check)
+void assert_log(const bool check)
 #endif
 {
 	if (check) {
 		return;
 	}
 
-#ifdef __cpp_lib_source_location
-	throw std::runtime_error(get_assert_message(location));
-#else
-	throw std::runtime_error("Assert failed.");
-#endif
+	log::log_error(get_assert_message(location));
 }
-
-#ifdef __cpp_lib_source_location
-extern void assert_log(const bool check, const std::source_location &location = std::source_location::current());
-#else
-extern void assert_log(const bool check);
-#endif
 
 }
