5109341398f49a00230e12510dd170f15ce78219
Andrettin <6322423+Andrettin@users.noreply.github.com>
2021-10-15 07:45:32 +0200

    Made it so geopoints are applied as 2x2 blocks

--- src/util/geocircle_util.cpp
+++ src/util/geocircle_util.cpp
@@ -38,10 +38,17 @@ void write_to_image(const QGeoCircle &geocircle, QImage &image, const QColor &co
 {
 	const QGeoCoordinate geocoordinate = geocircle.center();
 
-	const QPoint pixel_pos = map_projection->geocoordinate_to_point(wyrmgus::geocoordinate(geocoordinate), georectangle, image.size());
+	const QPoint base_pixel_pos = map_projection->geocoordinate_to_point(wyrmgus::geocoordinate(geocoordinate), georectangle, image.size());
 
-	if (pixel_pos.x() >= 0 && pixel_pos.y() >= 0 && pixel_pos.x() < image.width() && pixel_pos.y() < image.height()) {
-		geoshape::write_pixel_to_image(pixel_pos, color, image);
+	//write a 2x2 block so that the point can be expanded from in terrain generation, and so that it won't be removed if the terrain type does not allow single tiles
+	for (int x_offset = 0; x_offset <= 1; ++x_offset) {
+		for (int y_offset = 0; y_offset <= 1; ++y_offset) {
+			const QPoint pixel_pos = base_pixel_pos + QPoint(x_offset, y_offset);
+
+			if (pixel_pos.x() >= 0 && pixel_pos.y() >= 0 && pixel_pos.x() < image.width() && pixel_pos.y() < image.height()) {
+				geoshape::write_pixel_to_image(pixel_pos, color, image);
+			}
+		}
 	}
 }
 
