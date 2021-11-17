3bf5a907d68fd031827fa58bd0db094aed060e51
Andrettin <6322423+Andrettin@users.noreply.github.com>
2021-11-17 15:34:51 +0100

    Do not use a comma separator for years >= 1000 but < 10000

--- src/util/date_util.cpp
+++ src/util/date_util.cpp
@@ -34,7 +34,14 @@ namespace wyrmgus::date {
 
 std::string year_to_string(const int year)
 {
-	return number::to_formatted_string(std::abs(year));
+	const int abs_year = std::abs(year);
+
+	if (abs_year < 10000) {
+		//do not use a formatted string (which can have a comma separator) if the year number is less than 10,000
+		return std::to_string(abs_year);
+	}
+
+	return number::to_formatted_string(abs_year);
 }
 
 }
