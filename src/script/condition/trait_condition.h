1b5c9da500d7db6243b1ad02ea015daf127152f3
Andrettin <6322423+Andrettin@users.noreply.github.com>
2022-01-14 07:44:16 +0100

    Fixed parameter unused warning

--- src/script/condition/trait_condition.h
+++ src/script/condition/trait_condition.h
@@ -41,6 +41,7 @@ public:
 
 	virtual bool check(const CPlayer *player, const read_only_context &ctx, const bool ignore_units) const override
 	{
+		Q_UNUSED(player)
 		Q_UNUSED(ctx)
 		Q_UNUSED(ignore_units)
 
