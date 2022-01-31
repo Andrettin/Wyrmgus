c250ce10c8f40d543b4824854845cf424388cbb9
Andrettin <6322423+Andrettin@users.noreply.github.com>
2022-01-31 20:28:56 +0100

    Added population unit map

---
+++ src/population/population_unit_container.cpp
@@ -0,0 +1,41 @@
+//       _________ __                 __
+//      /   _____//  |_____________ _/  |______     ____  __ __  ______
+//      \_____  \\   __\_  __ \__  \\   __\__  \   / ___\|  |  \/  ___/
+//      /        \|  |  |  | \// __ \|  |  / __ \_/ /_/  >  |  /\___ |
+//     /_______  /|__|  |__|  (____  /__| (____  /\___  /|____//____  >
+//             \/                  \/          \//_____/            \/
+//  ______________________                           ______________________
+//                        T H E   W A R   B E G I N S
+//         Stratagus - A free fantasy real time strategy game engine
+//
+//      (c) Copyright 2022 by Andrettin
+//
+//      This program is free software; you can redistribute it and/or modify
+//      it under the terms of the GNU General Public License as published by
+//      the Free Software Foundation; only version 2 of the License.
+//
+//      This program is distributed in the hope that it will be useful,
+//      but WITHOUT ANY WARRANTY; without even the implied warranty of
+//      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//      GNU General Public License for more details.
+//
+//      You should have received a copy of the GNU General Public License
+//      along with this program; if not, write to the Free Software
+//      Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+//      02111-1307, USA.
+
+#include "stratagus.h"
+
+#include "population/population_unit_container.h"
+
+#include "population/population_unit.h"
+#include "population/population_unit_key.h"
+
+namespace wyrmgus {
+
+bool population_unit_compare::operator()(const population_unit *lhs, const population_unit *rhs) const
+{
+	return lhs->get_key() < rhs->get_key();
+}
+
+}
