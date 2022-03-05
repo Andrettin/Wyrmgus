ec57e124c12689082489ecec1b53796567151663
Andrettin <6322423+Andrettin@users.noreply.github.com>
2022-03-05 08:28:17 +0100

    Use __has_include to determine whether the <source_location> header should be included

--- src/pch.h
+++ src/pch.h
@@ -54,7 +54,7 @@
 #include <random>
 #include <set>
 #include <shared_mutex>
-#ifdef __cpp_lib_source_location
+#ifdef __has_include(<source_location>)
 #include <source_location>
 #endif
 #include <sstream>
