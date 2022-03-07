e20bc10bcd2abee89389b0e35b7d5cc3df89d60a
Andrettin <6322423+Andrettin@users.noreply.github.com>
2022-03-07 07:40:35 +0100

    Added operator to multiply two points

--- src/util/point_operators.h
+++ src/util/point_operators.h
@@ -26,6 +26,13 @@
 
 #pragma once
 
+inline constexpr const QPoint &operator *=(QPoint &lhs, const QPoint &rhs)
+{
+	lhs.setX(lhs.x() * rhs.x());
+	lhs.setY(lhs.y() * rhs.y());
+	return lhs;
+}
+
 inline constexpr const QPoint &operator /=(QPoint &lhs, const int rhs)
 {
 	lhs.setX(lhs.x() / rhs);
@@ -36,6 +43,13 @@ inline constexpr const QPoint &operator /=(QPoint &lhs, const int rhs)
 inline constexpr const QPoint &operator /=(QPoint &lhs, const int64_t rhs)
 {
 	return lhs /= static_cast<int>(rhs);
+}
+
+inline constexpr QPoint operator *(const QPoint &lhs, const QPoint &rhs)
+{
+	QPoint res(lhs);
+	res *= rhs;
+	return res;
 }
 
 inline constexpr QPoint operator *(const QPoint &lhs, const int64_t rhs)
