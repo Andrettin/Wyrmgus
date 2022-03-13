7344318e6e2fb86e691e416b62889c6cb85a4d92
Andrettin <6322423+Andrettin@users.noreply.github.com>
2022-03-13 20:51:41 +0100

    Don't generate metal rocks for every deposit map template unit

--- src/map/map_template.cpp
+++ src/map/map_template.cpp
@@ -2108,14 +2108,13 @@ void map_template::ApplyUnits(const QPoint &template_start_pos, const QPoint &ma
 
 			const int resource_amount = map_template_unit->get_resource_amount();
 
+			CUnit *unit = CreateUnit(unit_pos - unit_type->get_tile_center_pos_offset(), *unit_type, player, z, true, nullptr, true);
+
 			if (resource_amount > 0) {
-				CUnit *unit = CreateResourceUnit(unit_pos - unit_type->get_tile_center_pos_offset(), *unit_type, z, false);
 				unit->SetResourcesHeld(resource_amount);
 				unit->Variable[GIVERESOURCE_INDEX].Value = resource_amount;
 				unit->Variable[GIVERESOURCE_INDEX].Max = resource_amount;
 				unit->Variable[GIVERESOURCE_INDEX].Enable = 1;
-			} else {
-				CreateUnit(unit_pos - unit_type->get_tile_center_pos_offset(), *unit_type, player, z, true, nullptr, true);
 			}
 		}
 	}
