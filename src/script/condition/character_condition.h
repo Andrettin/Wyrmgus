64946b7b63bc60c3e0f7f131f1f03301abf86a2c
Andrettin <6322423+Andrettin@users.noreply.github.com>
2022-08-28 10:01:48 +0200

    Added support for the character scripted condition to relate to any character

--- src/script/condition/character_condition.h
+++ src/script/condition/character_condition.h
@@ -29,6 +29,8 @@
 #include "character.h"
 #include "script/condition/condition.h"
 #include "unit/unit.h"
+#include "util/string_conversion_util.h"
+#include "util/string_util.h"
 
 namespace wyrmgus {
 
@@ -38,7 +40,22 @@ class character_condition final : public condition<scope_type>
 public:
 	explicit character_condition(const std::string &value)
 	{
-		this->character = character::get(value);
+		if (string::is_bool(value)) {
+			this->is_character = string::to_bool(value);
+		} else {
+			this->character = character::get(value);
+		}
+	}
+
+	virtual void check_validity() const override
+	{
+		if (this->character == nullptr && !this->is_character.has_value()) {
+			throw std::runtime_error("\"character\" condition neither has a character, nor is set to relate to any character.");
+		}
+
+		if (this->character != nullptr && this->is_character.has_value()) {
+			throw std::runtime_error("\"character\" condition has both a character and is set to relate to any character.");
+		}
 	}
 
 	virtual bool check(const scope_type *scope, const read_only_context &ctx) const override
@@ -46,21 +63,56 @@ public:
 		Q_UNUSED(ctx);
 
 		if constexpr (std::is_same_v<scope_type, CPlayer>) {
-			return scope->HasHero(this->character);
+			if (this->is_character.has_value()) {
+				return !scope->Heroes.empty() == this->is_character.value();
+			} else {
+				return scope->HasHero(this->character);
+			}
 		} else if constexpr (std::is_same_v<scope_type, CUnit>) {
-			return scope->get_character() == this->character;
+			if (this->is_character.has_value()) {
+				return (scope->get_character() != nullptr) == this->is_character.value();
+			} else {
+				return scope->get_character() == this->character;
+			}
 		}
 	}
 
 	virtual std::string get_string(const size_t indent, const bool links_allowed) const override
 	{
-		Q_UNUSED(indent)
+		Q_UNUSED(indent);
 
-		return "Has the " + condition<scope_type>::get_object_string(this->character, links_allowed, this->character->get_full_name()) + " character";
+		std::string str;
+
+		if constexpr (std::is_same_v<scope_type, CPlayer>) {
+			if (this->is_character.has_value() && this->is_character.value() == false) {
+				str += "Does not have";
+			} else {
+				str += "Has";
+			}
+		} else if constexpr (std::is_same_v<scope_type, CUnit>) {
+			str += "Is";
+		}
+
+		if (this->is_character.has_value()) {
+			if constexpr (std::is_same_v<scope_type, CUnit>) {
+				if (this->is_character.value() == false) {
+					str += " not";
+				}
+			}
+
+			str += " a";
+		} else {
+			str += " the " + condition<scope_type>::get_object_string(this->character, links_allowed, this->character->get_full_name());
+		}
+
+		str += " character";
+
+		return str;
 	}
 
 private:
 	const wyrmgus::character *character = nullptr;
+	std::optional<bool> is_character;
 };
 
 }
